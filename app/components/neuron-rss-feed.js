b4a131c8124c056327ae0cc116089a5cb4d7847d
progand <lach@ukr.net>
2016-01-27 11:00:05 +0200

    [*] rss feed - random date

--- app/components/neuron-rss-feed.js
+++ app/components/neuron-rss-feed.js
@@ -19,11 +19,12 @@ export default Ember.Component.extend({
       rssFeed.push({
         image: `/img/feed/${_.random(0, 2)}.png`,
         title: `About ${firstWord} and ${secondWord} #${index}`,
-        date: new Date(),
+        date: new Date(_.random(Date.now() - 24*60*60*1000, Date.now())),
         text: `Lorem ipsum dolor sit amet <span class="text-success">${firstWord}</span>, consectetur adipiscing elit, sed do eiusmod tempor <span class="text-success">${secondWord}</span> incididunt ut labore et dolore magna aliqua <span class="text-success">${firstWord}</span>. Ut enim ad minim veniam, quis nostrud exercitation ullamco <span class="text-success">${secondWord}</span> laboris nisi ut aliquip ex ea commodo consequat...`
 
       })
     });
+
     return rssFeed;
   })
 });
