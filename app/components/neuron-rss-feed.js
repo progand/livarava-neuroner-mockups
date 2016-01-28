1843c285752bd118f2450e867d851caf2233fd74
progand <lach@ukr.net>
2016-01-28 10:16:25 +0200

    [*]  images moved to css

--- app/components/neuron-rss-feed.js
+++ app/components/neuron-rss-feed.js
@@ -19,16 +19,16 @@ export default Ember.Component.extend({
     if (_.isEmpty(this.get('model.url'))) {
       return rssFeed;
     }
-    [firstWord, secondWord, thirdWord] = _.chain(this.get('model.url')).words().without('feed', 'http', 'https', 'com', 'org').value();
+    [firstWord='news', secondWord='interesting', thirdWord='fascinating'] = _.chain(this.get('model.url')).words().without('feed', 'http', 'https', 'com', 'org').value();
 
     _.times(11, index => {
       rssFeed.push({
-        image: `/img/feed/${_.random(0, 2)}.png`,
+        imageIndex: _.random(0, 2),
         title: `About ${firstWord} and ${secondWord} #${index}`,
         date: new Date(_.random(Date.now() - 24 * 60 * 60 * 1000, Date.now())),
-        text: `Lorem ipsum dolor sit amet <span class="text-success">${firstWord}</span>, consectetur adipiscing elit, sed do eiusmod tempor <span class="text-success">${secondWord}</span> incididunt ut labore et dolore magna aliqua <span class="text-success">${firstWord}</span>. Ut enim ad minim veniam, quis nostrud exercitation ullamco <span class="text-success">${secondWord}</span> laboris nisi ut aliquip ex ea commodo consequat...`
+        text: `Lorem ipsum dolor sit amet <span class="text-success">${firstWord}</span>, consectetur adipiscing elit, sed do eiusmod tempor <span class="text-success">${secondWord}</span> incididunt ut labore et dolore magna aliqua <span class="text-success">${thirdWord}</span>. Ut enim ad minim veniam, quis nostrud exercitation ullamco <span class="text-success">${secondWord}</span> laboris nisi ut aliquip ex ea commodo consequat...`
 
-      })
+      });
     });
 
     return rssFeed;
