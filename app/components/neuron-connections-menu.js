6af7517d8d310c396d9bef2cf7745ab96c84f943
progand <lach@ukr.net>
2016-02-01 10:31:51 +0200

    [+] simple neuron form mockup

--- app/components/neuron-connections-menu.js
+++ app/components/neuron-connections-menu.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  activeForm: 'simple',
+  actions: {
+    setActiveForm(activeForm = null){
+      this.set('activeForm', activeForm);
+    },
+    addSimpleNeuron(){
+      console.log(this.get('simpleNeuronRawData'));
+    }
+  },
+  isSimpleFormActive: Ember.computed('activeForm', function () {
+    return this.get('activeForm') === 'simple';
+  })
 });
