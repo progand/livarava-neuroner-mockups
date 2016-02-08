97ab8f5f61a721056ebca7ca593a561df2178398
progand <lach@ukr.net>
2016-02-08 14:20:53 +0200

    [*]  comments component

--- app/components/neuron-additional-info.js
+++ app/components/neuron-additional-info.js
@@ -2,16 +2,26 @@ import Ember from 'ember';
 import NeuronDeterminator from '../mixins/neuron-type-determinator';
 
 export default Ember.Component.extend(NeuronDeterminator, {
+  showing: null,
+
   init(){
     this._super.apply(this, arguments);
-    this.set('showingConnections', !this.get('hasSpecificInfo'));
+    this.set('showing', this.get('hasSpecificInfo') ? 'info' : 'connections');
   },
+
   actions: {
-    showInfo(){
-      this.set('showingConnections', false);
-    },
-    showConnections(){
-      this.set('showingConnections', true);
+    show(whatToShow = 'info'){
+      this.set('showing', whatToShow);
     }
-  }
+  },
+
+  showingInfo: Ember.computed('showing', function () {
+    return this.get('showing') === 'info';
+  }),
+  showingComments: Ember.computed('showing', function () {
+    return this.get('showing') === 'comments';
+  }),
+  showingConnections: Ember.computed('showing', function () {
+    return this.get('showing') === 'connections';
+  }),
 });
