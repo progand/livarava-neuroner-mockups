ae1551947d44412a142deb5ddd70f9ce7ff9e1b9
progand <lach@ukr.net>
2016-02-16 15:42:14 +0200

    [*] api chenges

--- app/serializers/profile-feed-item.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.JSONAPISerializer.extend({
-  keyForAttribute: function(attr) {
-    return String(attr).toUpperCase();
-  }
-});
---
+++ tests/unit/adapters/profile-feed-item-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('adapter:profile-feed-item', 'Unit | Adapter | profile feed item', {
+  // Specify the other units that are required for this test.
+  // needs: ['serializer:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let adapter = this.subject();
+  assert.ok(adapter);
+});
