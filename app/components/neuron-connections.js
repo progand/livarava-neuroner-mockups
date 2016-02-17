09c0da7b074d30caf78ab7904da9eab3380a8222
00FullZero00 <00FullZero00@gmail.com>
2016-02-17 18:53:59 +0200

    [*]  neuron filter

--- app/components/neuron-connections.js
+++ app/components/neuron-connections.js
@@ -2,7 +2,10 @@ import Ember from 'ember';
 import ShowMoreListMixin from '../mixins/show-more-list';
 
 export default Ember.Component.extend(ShowMoreListMixin, {
-  items: Ember.computed('model.connections', function () {
-    return this.get('model.connections');
+  filter: '',
+  items: Ember.computed('model.connections', 'filter', function () {
+    return this.get('model.connections').filter((item) => {
+      return item && item.title && _.includes(item.title, this.get('filter'));
+    });
   })
 });
