434375e05c5aa58add98e6d72d18ae3f93a33509
progand <lach@ukr.net>
2016-02-17 12:57:24 +0200

    [*] Neuron model. Image and url are in api now

--- app/components/neuron-list-item.js
+++ app/components/neuron-list-item.js
@@ -4,8 +4,5 @@ import NeuronDeterminator from '../mixins/neuron-type-determinator';
 export default Ember.Component.extend(NeuronDeterminator, {
   route: Ember.computed('neuron.abstract', function () {
     return this.get('neuron.abstract') ? 'neuron' : 'search';
-  }),
-  image: Ember.computed('neuron.image', 'neuron.image_url', 'neuron.main_image_url', function () {
-    return this.get('neuron.image') || this.get('neuron.image_url') || this.get('neuron.main_image_url');
-  }),
+  })
 });
--- app/models/neuron.js
+++ app/models/neuron.js
@@ -1,5 +1,4 @@
 import DS from 'ember-data';
-import Ember from 'ember';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
@@ -15,20 +14,5 @@ export default DS.Model.extend({
   comments: DS.attr({defaultValue: []}),
   commentsCount: DS.attr('number'),
 
-  connections: DS.attr(),
-
-  linkUrl: DS.attr('string'),
-  imageUrl: DS.attr('string'),
-  videoUrl: DS.attr('string'),
-  projectUrl: DS.attr('string'),
-  productUrl: DS.attr('string'),
-  eventUrl: DS.attr('string'),
-  mainImageUrl: DS.attr('string'),
-
-  url: Ember.computed('type', function () {
-    return this.get(`${this.get('type')}Url`);
-  }),
-  image: Ember.computed('imageUrl', 'mainImageUrl', function () {
-    return this.get('imageUrl') || this.get('mainImageUrl');
-  })
+  connections: DS.attr()
 });
