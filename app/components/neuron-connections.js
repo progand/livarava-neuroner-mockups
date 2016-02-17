3828540df99dac6872e483e122a28763237000f1
progand <lach@ukr.net>
2016-02-17 20:01:33 +0200

    [*] case insensitive filter

--- app/components/neuron-connections.js
+++ app/components/neuron-connections.js
@@ -4,8 +4,6 @@ import ShowMoreListMixin from '../mixins/show-more-list';
 export default Ember.Component.extend(ShowMoreListMixin, {
   filter: '',
   items: Ember.computed('model.connections', 'filter', function () {
-    return this.get('model.connections').filter((item) => {
-      return item.title.toLowerCase().includes(this.get('filter').toLowerCase());
-    });
+    return this.get('model.connections').filter((item) => item.title.toLowerCase().includes(this.get('filter').toLowerCase()));
   })
 });
