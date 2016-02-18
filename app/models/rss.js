86cff142d460c4fa7d1c1cfe43b1871c47cb9f54
progand <lach@ukr.net>
2016-02-18 13:52:17 +0200

    [*] Rss model with computed properties.

--- app/models/rss.js
+++ app/models/rss.js
@@ -1,9 +1,14 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
   description: DS.attr('string'),
   text: DS.attr('string'),
   guid: DS.attr('string'),
-  pub_date: DS.attr('date')
+  pubDate: DS.attr('date'),
+  image: DS.attr('string', {defaultValue: 'image: "https://www.livarava.com/static/livarava/img/neurons/link.png'}),
+
+  url: Ember.computed.oneWay('guid'),
+  date: Ember.computed.oneWay('pub_date')
 });
