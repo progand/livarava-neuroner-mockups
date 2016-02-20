ecddbffa03d793a1f2a76f8b96fe4007ad826923
progand <lach@ukr.net>
2016-02-20 10:59:54 +0200

    [*]  fixed adapters

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,13 +2,10 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 export default DS.JSONAPIAdapter.extend({
+  host: 'https://dev.livarava.com',
   namespace: 'api/v2',
   pathForType: function (type) {
     var camelized = Ember.String.dasherize(type);
     return Ember.String.singularize(camelized);
-  },
-  buildURL: function (type, id, record) {
-    //call the default buildURL and then append a slash
-    return this._super(type, id, record) + '/';
   }
 });
--- app/adapters/feed.js
+++ app/adapters/feed.js
@@ -1,8 +1,5 @@
 import ApplicationAdapter from './application';
-import Ember from 'ember';
 
 export default ApplicationAdapter.extend({
-   pathForType: function() {
-    return 'neuroner';
-  }
+  host: ''
 });
