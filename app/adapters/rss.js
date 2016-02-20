6a04002545c7eac24586451c7dc90756eb0fa4e1
progand <lach@ukr.net>
2016-02-20 13:03:00 +0200

    [*]  rss endpoint

--- app/adapters/rss.js
+++ app/adapters/rss.js
@@ -3,5 +3,9 @@ import ApplicationAdapter from './application';
 export default ApplicationAdapter.extend({
    pathForType: function() {
     return 'neuroner';
+  },
+  buildURL: function (type, id, record) {
+    //call the default buildURL and then append a slash
+    return this._super(type, id, record) + '/';
   }
 });
